4e569a643947e76738f1d8c1dd8b6e7a5b92d61a
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2019-02-22 11:16:38 -0500

    Starting with Tabsjs. I think I'm doing this right. I guess we'll see

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,8 +5,13 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabData.map(tab => 
+          <Tab
+          tab={tab}
+          selectTabHandler={selectTabHandler}
+          selectedTab={selectedTab}
+          />
+          )}
       </div>
     </div>
   );
@@ -14,3 +19,7 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+
+/* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
