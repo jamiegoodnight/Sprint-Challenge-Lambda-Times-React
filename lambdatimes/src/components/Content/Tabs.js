9e793c72a4ddead034e55f1abb46156efaf6ad02
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2019-02-22 11:45:52 -0500

    Tabs being displayed.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {props.tabData.map(tab => 
+        {props.tabs.map(tab => 
           <Tab
           tab={tab}
-          selectTabHandler={selectTabHandler}
-          selectedTab={selectedTab}
+          selectedTab={props.selectedTab}
+          selectTabHandler={props.selectTabHandler}
           />
           )}
       </div>
